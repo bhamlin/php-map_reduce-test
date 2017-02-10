30cfcf1f874c608f7c9eec8b1355559c75570666
Billy J Hamlin <billy.hamlin@blackriver.coop>
2017-02-10 15:12:48 -0500

    Beautification

--- php.tst.php
+++ php.tst.php
@@ -4,12 +4,12 @@ function comma_collect( $u, $v ) {
     return "$u,$v";
 }
 
-$data = array( "lol", "this", "should", 9, "work\r");
+$data = array( "lol", "this", "should", 9, "work\r" );
 
-$dt = array_map("trim", $data);
+$dt = array_map( "trim", $data );
 
-$output = array_reduce( array_slice( $dt, 1 ), "comma_collect", $dt[0]);
-print($output);
-print("\n");
+$output = array_reduce( array_slice( $dt, 1 ), "comma_collect", $dt[0] );
+print( $output );
+print( "\n" );
 
  ?>

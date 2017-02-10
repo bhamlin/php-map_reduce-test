2626798e72dc638961e974e6c354808a1d6286af
Billy J Hamlin <billy.hamlin@blackriver.coop>
2017-02-10 15:11:23 -0500

    Yeah, I didn't need print_r

--- php.tst.php
+++ php.tst.php
@@ -9,7 +9,7 @@ $data = array( "lol", "this", "should", 9, "work\r");
 $dt = array_map("trim", $data);
 
 $output = array_reduce( array_slice( $dt, 1 ), "comma_collect", $dt[0]);
-print_r($output);
+print($output);
 print("\n");
 
  ?>
